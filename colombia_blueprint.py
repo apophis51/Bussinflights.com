751239e52e52548c2c692f2ed605fa059e4a9b94
apophis51 <malcolmxvernon@hotmail.com>
2022-12-26 20:25:24 -0500

    d

--- colombia_blueprint.py
+++ colombia_blueprint.py
@@ -19,26 +19,46 @@ def americanairlinesflightstocolombia():
 @colombia_blueprint.route("/flights-to-colombia-from-london/")
 def flightstocolombiafromlondon():
     return render_template("/colombia-flights/flights-to-colombia-from-london.html")
+#flights to medellin colombia
 
 @colombia_blueprint.route("/flights-to-colombia-medellin/")
 def flightstocolombiamedellin():
     return render_template("/colombia-flights/flights-to-colombia-medellin.html")
 
-@colombia_blueprint.route("/cheap-flights-to-medellin-colombia/")
-def cheapflightstomedellincolombia():
-    return render_template("/colombia-flights/cheap-flights-to-medellin-colombia.html")
 @colombia_blueprint.route("/flights-to-medellin-colombia-from-miami/")
 def flightstomedellincolombiafrommiami():
     return render_template("/colombia-flights/flights-to-medellin-colombia-from-miami.html")
 @colombia_blueprint.route("/flights-from-orlando-to-medellin-colombia/")
 def flightsfromorlandotomedellincolombia():
     return render_template("/colombia-flights/flights-from-orlando-to-medellin-colombia.html")
-@colombia_blueprint.route("/cheap-flights-from-miami-to-medellin-colombia/")
-def cheapflightsfrommiamitomedellincolombia():
-    return render_template("/colombia-flights/cheap-flights-from-miami-to-medellin-colombia.html")
 @colombia_blueprint.route("/cheap-flights-to-medellin-colombia-from-new-york/")
 def cheapflightstomedellincolombiafromnewyork():
     return render_template("/colombia-flights/cheap-flights-to-medellin-colombia-from-new-york.html")
-@colombia_blueprint.route("/flights-to-medellin-colombia-from-los-angeles")
+@colombia_blueprint.route("/flights-to-medellin-colombia-from-los-angeles/")
 def flightstomedellincolombiafromlosangeles():
     return render_template("/colombia-flights/flights-to-medellin-colombia-from-los-angeles.html")
+
+
+@colombia_blueprint.route("/flights-to-bogota-colombia/")
+def flightstobogotacolombia():
+    return render_template("/colombia-flights/flights-to-bogota-colombia.html")
+@colombia_blueprint.route("/flights-miami-to-bogota-colombia/")
+def flightsmiamitobogotacolombia():
+    return render_template("/colombia-flights/flights-miami-to-bogota-colombia.html")
+@colombia_blueprint.route("/flights-from-orlando-to-bogota-colombia/")
+def flightsfromorlandotobogotacolombia():
+    return render_template("/colombia-flights/flights-from-orlando-to-bogota-colombia.html")
+@colombia_blueprint.route("/flights-from-lax-to-bogota-colombia/")
+def flightsfromlaxtobogotacolombia():
+    return render_template("/colombia-flights/flights-from-lax-to-bogota-colombia.html")
+@colombia_blueprint.route("/flights-from-atlanta-to-bogota-colombia/")
+def flightsfromatlantatobogotacolombia():
+    return render_template("/colombia-flights/flights-from-atlanta-to-bogota-colombia.html")
+@colombia_blueprint.route("/flights-fort-lauderdale-to-bogota-colombia/")
+def flightsfortlauderdaletobogotacolombia():
+    return render_template("/colombia-flights/flights-fort-lauderdale-to-bogota-colombia.html")
+
+@colombia_blueprint.route("/flights-from-salt-lake-city-to-bogota-colombia/")
+def flightsfromsaltlakecitytobogotacolombia():
+    return render_template("/colombia-flights/flights-from-salt-lake-city-to-bogota-colombia.html")
+
